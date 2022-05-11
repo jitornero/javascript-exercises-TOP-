16a16acbc6bcdd11e477d29ed1d2ea84a7bf67f4
jitornero <tornerojuan@gmail.com>
2022-05-10 23:41:31 -0300

    test 01 pass - utilizando splice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,31 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, element) {
+
+    let array1 = array;
+
+    for (let i=0; i<array1.length; i++){
+        if (array1[i] == element){
+            array1.splice(i,1);
+            console.log(array1);
+            return array1;
+
+        }
+    }
+   
+
+    //let array2 = array.pop(element);
+
+
+    //return ;
+    console.log(array1, element);
 
 };
+
+
+
+
+removeFromArray([1, 2, 3, 4], 3)
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
