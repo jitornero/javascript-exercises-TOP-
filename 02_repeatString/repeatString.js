9c1ae9a884dd45d343d5bd67c93d894365285992
jitornero <tornerojuan@gmail.com>
2022-05-10 21:41:47 -0300

    hasta 03javascritp realizados

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+ let result= "";
+ if (num >= 0){
+    for (let i =0; i<num; i++){
+    result = result + string;
+    }    
+ }
+
+ else{
+     return "ERROR";
+ }
+  //console.log(result);
+   return result;
 
 };
 
+//repeatString("hey",6);
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+let array = string.split("");
+array.reverse();
+let together = array.join("");
+
+return together;
+//console.log(array);
+//console.log(together);
 
 };
+
+
+
+reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
