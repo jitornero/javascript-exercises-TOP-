4c545bf03f4f47aff4167c95482cdcdedbd644cb
jitornero <tornerojuan@gmail.com>
2022-05-12 23:26:11 -0300

    Guardado antes de la prueba con slice(arguments)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,31 +1,24 @@
-const removeFromArray = function(array, element) {
+const removeFromArray = function(array, elem1, elem2) {
 
     let array1 = array;
+    let elements = [elem1, elem2]
 
     for (let i=0; i<array1.length; i++){
-        if (array1[i] == element){
+        if (array1[i] == elem1){
             array1.splice(i,1);
             console.log(array1);
             return array1;
-
         }
     }
-   
+
 
     //let array2 = array.pop(element);
-
-
     //return ;
-    console.log(array1, element);
+    console.log(array1, elem1);
 
 };
 
-
-
-
 removeFromArray([1, 2, 3, 4], 3)
-
-
 
 // Do not edit below this line
 module.exports = removeFromArray;
