8ac840c9173d89b48e6ac577c42402c8ef175b87
jitornero <tornerojuan@gmail.com>
2022-05-12 23:50:21 -0300

    volver a la version anterior a esta porque se rompio XD

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,24 +1,52 @@
-const removeFromArray = function(array, elem1, elem2) {
+const removeFromArray = function(array, ...arguments) {
 
     let array1 = array;
-    let elements = [elem1, elem2]
+    //let elements = [elem1, elem2];
+    let normalArray = Array.prototype.slice.call(arguments);
+    console.log(normalArray);
 
     for (let i=0; i<array1.length; i++){
-        if (array1[i] == elem1){
-            array1.splice(i,1);
-            console.log(array1);
-            return array1;
+        for (let j=0; j<normalArray.length;j++){
+            if (array1[i] == normalArray[j]){
+                array1.splice(i,1);
+                console.log(array1);
+                return array1;
+                
+            }
         }
+        
+        
     }
 
 
     //let array2 = array.pop(element);
     //return ;
-    console.log(array1, elem1);
+    //console.log(array1, normalArray);
 
 };
 
-removeFromArray([1, 2, 3, 4], 3)
+removeFromArray([1, 2, 3, 4], 3, 4)
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+
+
+
+
+
+
+
+// LLAMAR ARGUMENTOS, PARA "OBTENER" ARRAY DESDE A PARTIR DE UN ELEMENTO ESPECIFICO,
+//FIJARSE EL PRIMER EJEMPLO: ARGUMENTS (EL ARRAY DE ARGUMENTS), "1" ES EL INDEX DE ESE ARGUMENTS.
+//EN ESTE CASO, TOMARIA A PARTIR DEL B, PORUQE A ES EL ARGUMENTO [0]
+    // function f(a, b) {
+    //     let normalArray = Array.prototype.slice.call(arguments,1)
+    //     // -- or --
+    //     let normalArray = [].slice.call(arguments)
+    //     // -- or --
+    //     let normalArray = Array.from(arguments)
+      
+
+
