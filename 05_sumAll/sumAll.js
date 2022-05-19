fc982bc5e23b14038efb747f342a02e14058e552
jitornero <tornerojuan@gmail.com>
2022-05-19 00:31:32 -0300

    todos los test pass excepto el último

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,41 @@
-const sumAll = function() {
+
+const sumAll = function(x,y) {
+    let array = [];
+    if (x<0 || y<0 ){
+        return "ERROR";
+    }
+
+    else if (typeof(x) == 'string' || typeof(y) == 'string'){
+        return "ERROR";
+
+    }
+
+    // else if (typeof(x) != 'string' || typeof(y) != 'string'){
+    //     return "ERROR";
+
+    // }
+
+    else if (x<y){ 
+        for (let i=x; i <= y;i++){
+            array.push(i);
+        }
+    }
+    else {
+        for (let i=y; i <= x;i++){
+            array.push(i);
+        }
+    }
+
+    let reduceArray = array.reduce((acumulador, numero) => {return acumulador + numero}, 0);
+    console.log(reduceArray);
+    return reduceArray;
+
 
 };
 
+
+
+
+//sumAll(2,10);
 // Do not edit below this line
 module.exports = sumAll;
