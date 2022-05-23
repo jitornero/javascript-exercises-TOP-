dc0440508e3b0d080ea5ca2a5db8e179b8cd5995
jitornero <tornerojuan@gmail.com>
2022-05-23 00:43:34 -0300

    pass los 06- TENER EN CUENTA EL TEST DE JEST EN ESTE EXERCISE PARA TOMAR DE EJEMPLO YA QUE SON 2 TEST CASES (2 FUNCIONES POR SEPARADO)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,18 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+let celsius = (temp-32) * (5/9);
+let round = Math.round(celsius *10) /10;
+console.log("ftoc",round);
+return round;
 
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+let farenheit = (temp * 9/5) +32; 
+let round = Math.round(farenheit *10) /10;
+console.log("ctof", round)
+return round;
 };
+
 
 // Do not edit below this line
 module.exports = {
