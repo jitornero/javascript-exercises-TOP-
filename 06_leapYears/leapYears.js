280d922cb5686c79333f82f36bb609ec5a728dce
jitornero <tornerojuan@gmail.com>
2022-05-22 22:50:45 -0300

    pass hasta el test 1600 que me devuelve false cuando expected es true y todas las condiciones parecerian estar bien

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,27 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+if (year%4==0 ) {
+    if (year%100  && year%400 ){
+        return true;
+    }
+
+    else {return false}
+}
+
+else {
+    return false
+}
+
 
 };
 
+
+
+
 // Do not edit below this line
 module.exports = leapYears;
+
+
+// le doy un año ->
+// evalua si es leap year o no ->
+// retorna true or false según resultado
