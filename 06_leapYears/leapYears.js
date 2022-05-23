dcbcdbaf27fe9e114e9730fce06ac6fb39dd89e9
jitornero <tornerojuan@gmail.com>
2022-05-22 23:18:09 -0300

    pass test 06/1600. La solucion fue dividir las condiciones && y ponerlas en if anidadas (separandolas)

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,15 +1,20 @@
 const leapYears = function(year) {
 
 if (year%4==0 ) {
-    if (year%100  && year%400 ){
-        return true;
-    }
 
-    else {return false}
+    if (year%100==0) {
+        if (year%400 ==0) {
+            return true
+        }
+        else { return false
+        }
+    }  
+
+    else {return true}
+
 }
-
 else {
-    return false
+   return false
 }
 
 
